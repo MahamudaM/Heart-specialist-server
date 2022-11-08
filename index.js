f3fb4ffa04eccfb1092978d9763609f00b4818f7
unknown <mahamuda3052@gmail.com>
2022-11-08 17:54:41 +0600

    set up mongodb

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express =require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
-
+require('dotenv').config();
 // middle ware
 app.use(cors())
 app.use(express.json())
@@ -11,7 +12,12 @@ app.get('/',(req,res)=>{
     res.send('assingment server runing')
 })
 
+// monogo db confi
 
+
+const uri = `mongodb+srv://${process.env.DV_USER_NAME}:${process.env.USER_PASSWORD}@cluster0.4jfewjr.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
 
 
 
