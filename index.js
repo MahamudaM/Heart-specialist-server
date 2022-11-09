57c0a5f04aab474e67e65b0966926a0097becee4
unknown <mahamuda3052@gmail.com>
2022-11-09 06:18:46 +0600

    create review api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express =require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -22,11 +22,27 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         const serviceCollection = client.db("doctorA11").collection("services");
+        const reviewCollection = client.db("doctorA11").collection("review");
+
         app.get('/services',async(req,res)=>{
             const query= {}
             const cursor = serviceCollection.find(query)
             const services=await cursor.toArray();
             res.send(services)
+        })
+        
+        app.get('/services/:id',async(req,res)=>{
+            const id=req.params.id
+            const query= {_id:ObjectId(id)}
+            const services= await serviceCollection.findOne(query)
+            
+            res.send(services)
+        })
+
+        app.post('/review',async(req,res)=>{
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result)
         })
     }
     finally{
