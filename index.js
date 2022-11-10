5a4ede0da45aeaad926fafbd1152412174b425d8
unknown <mahamuda3052@gmail.com>
2022-11-10 10:22:09 +0600

    creat specific route api for add service page

--- index.js
+++ index.js
@@ -17,14 +17,14 @@ app.get('/',(req,res)=>{
 
 
 const uri = `mongodb+srv://${process.env.DV_USER_NAME}:${process.env.USER_PASSWORD}@cluster0.4jfewjr.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
 
 async function run(){
     try{
         const serviceCollection = client.db("doctorA11").collection("services");
         const reviewCollection = client.db("doctorA11").collection("review");
-
+        const orderServiceCollection = client.db("doctorA11").collection('orderedService')
         app.get('/services',async(req,res)=>{
             const query= {}
             const cursor = serviceCollection.find(query)
@@ -39,7 +39,26 @@ async function run(){
             
             res.send(services)
         })
-        // get review form database
+        // create api for add service
+app.post('/addService',async(req,res)=>{
+    const services = req.body;
+    const result = await orderServiceCollection.insertOne(services)
+    res.send(result)
+})
+
+// get specific user review from database
+app.get('/addService',async(req,res)=>{
+    let query={}
+    if(req.query.email){
+         query={
+            email:req.query.email
+         }
+    }
+    const cursor = orderServiceCollection.find(query)
+    const orderService = await cursor.toArray()
+    res.send(orderService)
+})
+
         // get specific user review from database
         app.get('/review',async(req,res)=>{
             let query={}
@@ -56,9 +75,24 @@ async function run(){
 // create review api to send database
         app.post('/review',async(req,res)=>{
             const review = req.body;
-            const result = await reviewCollection.insertOne(review);
-            res.send(result);
+            const result = await reviewCollection.insertOne(review);           
+            res.send( result);
         })
+
+
+
+
+        // get all review by id
+        // app.get('/review/:id',async(req,res)=>{
+        //     const id=req.params.id
+        //     const query= {_id:ObjectId(id)}
+        //     const result= await reviewCollection.findOne(query)
+            
+        //     res.send(result)
+        // })
+
+
+
         // create review delete api
         app.delete('/review/:id',async(req,res)=>{
             const id = req.params.id;
@@ -67,18 +101,44 @@ async function run(){
             res.send(result);
         })
         // updater review
-        app.patch('/review/:;id',async(req,res)=>{
+
+        app.patch('/review/:id',async(req,res)=>{
             const id = req.params.id;
-            const status = req.body.status
+            // const status = req.body.message
             const query = {_id:ObjectId(id)}
             const updatedDoc = {
-                $set:{
-                    status:status
-                }
+                $set:{message:req.body.message}
             }
             const result = await reviewCollection.updateOne(query,updatedDoc)
             res.send(result)
         })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // app.patch('/review/:id',async(req,res)=>{
+        //     const id = req.params.id;
+        //     const status = req.body.status
+        //     const query = {_id:ObjectId(id)}
+        //     const updatedDoc = {
+        //         $set:{
+        //             status:status
+        //         }
+        //     }
+        //     const result = await reviewCollection.updateOne(query,updatedDoc)
+        //     res.send(result)
+        // })
     }
     finally{
        
